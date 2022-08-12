6c7b666d3e191ec0b7ca113efe299ba68bfaaf92
Thiara de Alwis <thiaradeal@gmail.com>
2022-08-12 16:30:41 +0100

    added color options

---
+++ index.js
@@ -0,0 +1,70 @@
+const container = document.getElementById("container")
+let gridElement
+let numRow
+let numCol
+let toChange
+
+makeGrid(16, 16);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    var size = window.prompt("Enter no. of squares per side")
+    clearGrid()
+    makeGrid(size, size)
+})
+
+const red = document.querySelector('#red');
+red.addEventListener('click', () => {
+    choiceColor = "red"
+})
+
+const blue = document.querySelector('#blue');
+blue.addEventListener('click', () => {
+    choiceColor = "blue"
+})
+
+const green = document.querySelector('#green');
+green.addEventListener('click', () => {
+    choiceColor = "green"
+})
+
+
+
+
+
+function makeGridElement (numElements) {
+    for (let i = 0; i < numElements; i++) {
+        gridElement = document.createElement('div');
+        gridElement.classList.add('gridElement');
+        container.appendChild(gridElement);
+    
+    }
+}
+
+function makeGrid (numRow, numCol) {
+    container.style.setProperty('--numRows', numRow);
+    container.style.setProperty('--numCols', numCol);
+    for (let j = 0; j<numRow; j++) {
+        makeGridElement(numCol)
+    }
+}
+
+function clearGrid () {
+    element = document.getElementById('gridElement');
+    while (container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
+}
+
+container.addEventListener('mouseover', function(e){
+    if (e.target.matches('.gridElement')){
+        e.target.style.backgroundColor = choiceColor
+    }
+});
+
+
+
+
+
+
+
